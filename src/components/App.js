16955fe6c2bf6a4ca7537a5a6bfc701eb4a3eb7a
RaxRai <razzz.rai@gmail.com>
2020-10-31 18:43:16 +0530

    commit 3

--- src/components/App.js
+++ src/components/App.js
@@ -5,17 +5,20 @@ function App()
 {
 	const [ tasks, setTasks] = useState([]);
 	const [text , setText] = useState("");
+	const [editText , setEditText] = useState("");
 	const handleChange = (e)=>{
 		setText(e.target.value);
 	}
 	const handleEditChange = (e)=>{
-		setText(e.target.value);
+		setEditText(e.target.value);
 	}
 	const handleSaveTask=(index)=>{
+		if(editText === "") return;
 		let tasksCopy = [...tasks];
-		tasksCopy[index].name = text;
+		tasksCopy[index].name = editText;
 		tasksCopy[index].edit = false;
 		setTasks(tasksCopy);
+		setEditText("");
 	}
 	const handleAdd =()=>{
 		if(text === "") return;
@@ -39,27 +42,27 @@ function App()
 	
 	return (
 	<div id="main">
-		<textarea rows="3" id="task" onChange={handleChange}/>
-		<button id="btn" onClick={handleAdd}>Add</button>
+		<textarea rows="3" id="task" onChange={handleChange} value={text}/>
+		<button id="btn" onClick={handleAdd} disabled={text.length == 0}>Add</button>
 
 		{tasks.map((task, index)=>(
 			<div key={index} className="list">
-			<p id={index}>{task.name}</p>
+				<div id={index}>{task.name}</div>
+				
+				{!task.edit? (
+					<>
+					<button className="edit" onClick={()=>handleEdit(index)}>Edit</button>
+					<button className="delete" onClick={()=>handleDelete(index)}>Delete</button>
 			
-			{!task.edit? (
-				<div>
-				<button className="edit" onClick={()=>handleEdit(index)}>Edit</button>
-				<button className="delete" onClick={()=>handleDelete(index)}>Delete</button>
-		
-				</div>) : (
-					<div>
-						<textarea onChange={handleEditChange} className="editTask"/>
-						<button className="saveTask" onClick={()=>handleSaveTask(index)}>Save Task</button>
-					</div>
-				)
+					</>) : (
+						<>
+							<textarea onChange={handleEditChange} className="editTask" value={editText}/>
+							<button className="saveTask" onClick={()=>handleSaveTask(index)} disabled={editText.length == 0}>Save Task</button>
+						</>
+					)
 
-			}
-			<br />
+				}
+				<br />
 			</div>
 		))}
 	</div>
