6e835fa4f343b1e2e0d9e2964a6dfbf99c370621
RaxRai <razzz.rai@gmail.com>
2020-10-31 18:23:04 +0530

    commit 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,67 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./../styles/App.css";
 
 function App() 
 {
+	const [ tasks, setTasks] = useState([]);
+	const [text , setText] = useState("");
+	const handleChange = (e)=>{
+		setText(e.target.value);
+	}
+	const handleEditChange = (e)=>{
+		setText(e.target.value);
+	}
+	const handleSaveTask=(index)=>{
+		let tasksCopy = [...tasks];
+		tasksCopy[index].name = text;
+		tasksCopy[index].edit = false;
+		setTasks(tasksCopy);
+	}
+	const handleAdd =()=>{
+		if(text === "") return;
+		let tasksCopy = [...tasks];
+		tasksCopy.push({ name: text, edit: false});
+		setTasks(tasksCopy);
+		setText("");
+	}
+	const handleEdit=(index)=>{
+		let tasksCopy = [...tasks];
+		tasksCopy[index].edit = true; 
+		setTasks(tasksCopy);
+
+	}
+	const handleDelete=(index)=>{
+		let tasksCopy = [...tasks];
+		let taskText = text;
+		tasksCopy.splice(index,1);
+		setTasks(tasksCopy);
+	}
+	
 	return (
 	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
+		<textarea rows="3" id="task" onChange={handleChange}/>
+		<button id="btn" onClick={handleAdd}>Add</button>
+
+		{tasks.map((task, index)=>(
+			<div key={index}>
+			<p id={index}>{task.name}</p>
+			
+			{!task.edit? (
+				<div>
+				<button className="edit" onClick={()=>handleEdit(index)}>Edit</button>
+				<button className="delete" onClick={()=>handleDelete(index)}>Delete</button>
+		
+				</div>) : (
+					<div>
+						<textarea onChange={handleEditChange} className="editTask"/>
+						<button className="saveTask" onClick={()=>handleSaveTask(index)}>Save Task</button>
+					</div>
+				)
+
+			}
+			<br />
+			</div>
+		))}
 	</div>
 	);
 }
